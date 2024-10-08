f24b07fecadef0c9598bcc67bbcc8c9606f13636
Yoshiaki Iwamoto <yoshiaki.iwamoto@gmail.com>
2024-10-08 11:33:04 +0900

    課題1を修正

--- uru.py
+++ uru.py
@@ -1,5 +1,10 @@
 year = int(input())
-if year % 4 == 0 :
-    print(str(year) + "年はうるう年である。")
-else :
+
+if year % 4 != 0:
     print(str(year) + "年はうるう年ではない。")
+elif year % 100 != 0:
+    print(str(year) + "年はうるう年である。")
+elif year % 400 != 0:
+    print(str(year) + "年はうるう年ではない。")
+else:
+    print(str(year) + "年はうるう年である。")
